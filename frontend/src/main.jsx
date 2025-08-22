c333b684b188a7b5a5665c8fbccae80303555b95
Shivansha pathak <Shivanshapathak23@gmail.com>
2025-08-23 02:28:54 +0530

    Switch to HashRouter and update build configuration for GitHub Pages

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,16 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom'
+import { HashRouter } from 'react-router-dom'
 import ShopContextProvider from './context/ShopContext.jsx'
 import { ThemeProvider } from './context/ThemeContext.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <HashRouter>
     <ShopContextProvider>
       <ThemeProvider>
         <App />
       </ThemeProvider>
     </ShopContextProvider>
-  </BrowserRouter>,
+  </HashRouter>,
 )
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -5,14 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   base: '/GreenCart/',
-  server: {port:5173},
+  server: {
+    port: 5173
+  },
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
     emptyOutDir: true,
     rollupOptions: {
       output: {
-        manualChunks: undefined
+        manualChunks: undefined,
+        assetFileNames: 'assets/[name].[hash].[ext]',
+        chunkFileNames: 'assets/[name].[hash].js',
+        entryFileNames: 'assets/[name].[hash].js'
       }
     }
   }
