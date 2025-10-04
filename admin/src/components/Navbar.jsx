78d294b1457c7e1ebd42bd55f805a7e7a092f4ae
Shivansha pathak <Shivanshapathak23@gmail.com>
2025-10-05 02:03:16 +0530

    Remove FOREVER logo from admin panel navbar

--- admin/src/components/Navbar.jsx
+++ admin/src/components/Navbar.jsx
@@ -5,8 +5,7 @@ const Navbar = ({setToken}) => {
   return (
     <div className='flex items-center py-2 px-[4%] justify-between'>
         <div className="flex items-center">
-          <img className='w-[max(10%,80px)]' src={assets.logo} alt="" />
-          <span className="text-green-600 font-semibold ml-2 text-lg">GreenCart Admin</span>
+          <span className="text-green-600 font-bold text-xl">GreenCart Admin</span>
         </div>
         <button onClick={()=>setToken('')} className='bg-green-600 text-white px-5 py-2 sm:px-7 sm:py-2 rounded-full text-xs sm:text-sm'>Logout</button>
     </div>
